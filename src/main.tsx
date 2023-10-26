fc71fc46e9eeadbbff8b9fe57ba8023955ffe19b
Mcharo23 <mcharoprofg23@gmail.com>
2023-10-26 21:52:42 +0300

    added style

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Notifications } from "@mantine/notifications";
